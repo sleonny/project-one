e94d8294bc819234def56c7bbdadd8b83a8760b2
Sean L <leonardmsean@gmail.com>
2023-03-22 20:07:48 -0400

    got quote to load on card finally

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,73 @@
 
-
-
-
 function getRandomQuote() {
-    // Set the API endpoint URL
-    var urlQuote = "https://quote-garden.onrender.com/api/v3/quotes/random";
-  
-    // Send the HTTP GET request to the API endpoint
-    fetch(urlQuote)
-      .then(response => response.json())
-      .then(data => {
-        // Print the quote to the console
-        var quote = data.data[0];
-        console.log(`${quote.quoteText} - ${quote.quoteAuthor}`);
-     
-        var textarea = document.getElementById('textarea1');
-        textarea.value = quote.quoteText;
-       }) }
-  
-  // Call the function to retrieve a random quote
-  getRandomQuote();
+  // Set the API endpoint URL
+  var urlQuote = "https://quote-garden.onrender.com/api/v3/quotes/random";
+
+  // Send the HTTP GET request to the API endpoint
+  fetch(urlQuote)
+    .then(response => response.json())
+    .then(data => {
+      // Print the quote to the console
+      var quote = data.data[0].quoteText;
+      console.log(`${quote}`);
+   
+      var quoteElement = document.getElementById('quote');
+      quoteElement.textContent = quote;
+     }) }
+
+// Call the function to retrieve a random quote
+getRandomQuote();
 
 
-      
-  function getDefinition() {
-  var searchForm = document.getElementById() 
-  searchForm.addEventListener('submit', event => {
-  event.preventDefault(); 
- const searchForm = document.getElementById('search-form');
- var searchTerm = document.getElementById('search-input').value;
- var urlDictionary = `https://api.dictionaryapi.dev/api/v2/entries/en/${encodeURIComponent(searchTerm)}`;
- fetch(urlDictionary)
- .then(response => response.json())
- .then(data => {
- // Get Definition of Word
- var definition = data.data[0];
- console.log(definition);
- })
- .catch(error => console.error(error));
- })}
-  
- getDefinition();
-  
-  
-
-
-
-
-//BELOW NEED TO BRACKET AND ATTACH HTML ELEMENT TO END OF DOCUMENT.
-   // displayResults(data) 
-        var word = data.word;
-        var displayWord = document;
- function getDefinition() {
-     
-  searchForm.addEventListener('submit', event => {
-  event.preventDefault(); 
- const searchForm = document.getElementById('search-form');
- var searchTerm = document.getElementById('search-input').value;
- var urlDictionary = `https://api.dictionaryapi.dev/api/v2/entries/en/${encodeURIComponent(searchTerm)}`;
- fetch(urlDictionary)
- .then(response => response.json())
- .then(data => {
- 
- var definition = data.data[0];
- console.log(definition);
- })
- .catch(error => console.error(error));
- })}
-
-       var displayWord = document;
-        var orgin = data.orgin;
-        var displayOrgin = document;
-        var meanings = data.meanings;
-        var displayMeanings = document;
-        var partOfSpeech = data.partOfSpeech;
-        var displaypartofSpeech = document;
     
+// function getDefinition() {
+   
+ // searchForm.addEventListener('submit', event => {
+   // event.preventDefault(); 
+    //const searchForm = document.getElementById('search-form');
+    //var searchTerm = document.getElementById('search-input').value;
+    //var urlDictionary = `https://api.dictionaryapi.dev/api/v2/entries/en/${encodeURIComponent(searchTerm)}`;
+    //fetch(urlDictionary)
+    //.then(response => response.json())
+    //.then(data => {
+      // Get Definition of Word
+      //var definition = data.data[0];
+      //console.log(definition);
+    //})
+    //.catch(error => console.error(error));
+//})}
+
+
+
+
+
+
+
+
+      // var displayWord = document;
+      // var orgin = data.orgin;
+      // var displayOrgin = document;
+      // var meanings = data.meanings;
+      // var displayMeanings = document;
+      // var partOfSpeech = data.partOfSpeech;
+      // var displaypartofSpeech = document;
+  
+var saveButton = document.querySelector('#quote-save-btn');
+var input = document.querySelector('#input');
+
+
+
+function saveData() {
+
+localStorage.setItem("value", input.value);
+var data = localStorage.getItem("value");
+console.log(data);
+
+}
+
+saveButton.addEventListener("click", saveData);
+      
+      
+
+
 
