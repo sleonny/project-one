a3133ab825594dc656d01fd27b3fa3c68958398e
Sean L <leonardmsean@gmail.com>
2023-03-23 19:12:53 -0400

    testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 function getRandomQuote() {
   // Set the API endpoint URL
   var urlQuote = "https://quote-garden.onrender.com/api/v3/quotes/random";
@@ -12,7 +11,15 @@ function getRandomQuote() {
       var author = data. data[0].quoteAuthor;
       console.log(`${quote}`);
       
+   
+      var quoteElement = document.getElementById('quote');
+      quoteElement.textContent = quote;
+      var authorElement = document.getElementById('author');
+      authorElement.textContent = author;
+     }) }
 
+// Call the function to retrieve a random quote
+getRandomQuote();
 
 var button = document.getElementById('search');
 
@@ -22,7 +29,31 @@ button.addEventListener('click', function() {
 });
 
 
+function getDefinition() {
+  console.log("getDefinition function called"); 
+     var word = document.getElementById('search').value;
+     console.log(`word value: ${word}`);
+    var urlDictionary = `https://api.dictionaryapi.dev/api/v2/entries/en/${encodeURIComponent(word)}`;
+    console.log(urlDictionary);
+    fetch(urlDictionary)
+    .then(response => response.json())
+    .then(data => {
+      var definitions = data[0].meanings[0].definitions;
+      var definitionString = '';
+      definitions.forEach(function(definition, index) {
+        definitionString += `${index + 1}. ${definition.definition}\n`;
+      });
+      console.log(definitionString);
+      var displayDefinition = document.getElementById('definition');
+      displayDefinition.textContent = definitionString;
+    })
+    
+    .catch(error => console.error(error));
+}
 
+   
+
+  
 var saveButton = document.querySelector('#quote-save-btn');
 var input = document.querySelector('#input');
 
@@ -30,9 +61,15 @@ var input = document.querySelector('#input');
 
 function saveData() {
 
+localStorage.setItem("value", input.value);
+var data = localStorage.getItem("value");
+console.log(data);
 
 }
 
 saveButton.addEventListener("click", saveData);
+      
+      
+
 
 
